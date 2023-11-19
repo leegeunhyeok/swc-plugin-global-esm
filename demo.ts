d0fc6a037d04e0b360c2e0546fea78fe87f7eeab
leegeunhyeok <dev.ghlee@gmail.com>
2023-11-19 14:35:50 +0900

    feat: setup demo environment

---
+++ demo.ts
@@ -0,0 +1,60 @@
+import { resolve } from 'node:path';
+import { transform } from '@swc/core';
+import highlight from 'cli-highlight';
+
+const inputCode =`
+import React, { useState, useEffect } from 'react';
+import { Container, Section, Button, Text } from '@app/components';
+import { useCustomHook } from '@app/hooks';
+
+export function MyComponent (): JSX.Element {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('effect');
+  }, []);
+
+  useCustomHook();
+
+  return (
+    <Container>
+      <Section>
+        <Text>{'Hello, World'}</Text>
+      </Section>
+      <Section>
+        <Text>{count}</Text>
+      </Section>
+      <Section>
+        <Button onPress={() => setCount((v) => v + 1)}>
+          <Text>{'Press Me'}</Text>
+        </Button>
+      </Section>
+    </Container>
+  );
+};
+
+export default class {}
+`;
+
+;(async () => {
+  const { code: outputCode } = await transform(inputCode, {
+    isModule: true,
+    filename: 'demo.tsx',
+    jsc: {
+      parser: {
+        syntax: 'typescript',
+        tsx: true,
+      },
+      experimental: {
+        plugins: [
+          [resolve('target/wasm32-wasi/release/swc_plugin_global_esm.wasm'), {
+            runtimeModule: true,
+          }],
+        ],
+      },
+      externalHelpers: false,
+    },
+  });
+
+  console.log(highlight(outputCode, { language: 'js' }));
+})();
